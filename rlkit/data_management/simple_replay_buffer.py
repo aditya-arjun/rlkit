810b24529fa2be195da6a5f86562869858b9d062
aditya-arjun <aditya.arjun123@gmail.com>
2021-03-27 11:57:22 -0500

    replace reply buffer with np.ones to force memory allocation

--- rlkit/data_management/simple_replay_buffer.py
+++ rlkit/data_management/simple_replay_buffer.py
@@ -17,22 +17,22 @@ class SimpleReplayBuffer(ReplayBuffer):
         self._observation_dim = observation_dim
         self._action_dim = action_dim
         self._max_replay_buffer_size = max_replay_buffer_size
-        self._observations = np.zeros((max_replay_buffer_size, observation_dim))
+        self._observations = np.ones((max_replay_buffer_size, observation_dim))
         # It's a bit memory inefficient to save the observations twice,
         # but it makes the code *much* easier since you no longer have to
         # worry about termination conditions.
-        self._next_obs = np.zeros((max_replay_buffer_size, observation_dim))
-        self._actions = np.zeros((max_replay_buffer_size, action_dim))
+        self._next_obs = np.ones((max_replay_buffer_size, observation_dim))
+        self._actions = np.ones((max_replay_buffer_size, action_dim))
         # Make everything a 2D np array to make it easier for other code to
         # reason about the shape of the data
-        self._rewards = np.zeros((max_replay_buffer_size, 1))
+        self._rewards = np.ones((max_replay_buffer_size, 1))
         # self._terminals[i] = a terminal was received at time i
-        self._terminals = np.zeros((max_replay_buffer_size, 1), dtype='uint8')
+        self._terminals = np.ones((max_replay_buffer_size, 1), dtype='uint8')
         # Define self._env_infos[key][i] to be the return value of env_info[key]
         # at time i
         self._env_infos = {}
         for key, size in env_info_sizes.items():
-            self._env_infos[key] = np.zeros((max_replay_buffer_size, size))
+            self._env_infos[key] = np.ones((max_replay_buffer_size, size))
         self._env_info_keys = env_info_sizes.keys()
 
         self._top = 0
